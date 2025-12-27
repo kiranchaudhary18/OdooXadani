65af24751755cc20d9d005c14a0c31926f1a8768
shubhamiscodding <shubham.modi.cg@gmail.com>
2025-12-27 10:44:32 +0530

    added error handling in app.js for better stability and user feedback.

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,19 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("GearGuard API is running 🚀");
+});
+
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/maintenance", require("./routes/maintenance.routes"));
 app.use("/api/equipment", require("./routes/equipment.routes"));
 app.use("/api/reports", require("./routes/report.routes"));
 app.use("/api/teams", require("./routes/team.routes"));
 
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: "Internal Server Error" });
+});
 
 module.exports = app;
